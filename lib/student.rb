cfd9dc74467d1cc5add9a939e09737c5e7c97fce
Miguel Gonzalez <sb.mgonzalez1@gmail.com>
2017-12-07 03:13:48 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,30 +1,41 @@
 require_relative "../config/environment.rb"
 
 class Student
-  attr_accessor :name, :grade
-  attr_reader :id
+  attr_accessor :name, :grade, :id
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-  def initialize(id=nil, name, grade)
-    @id = id
-    @name = name
-    @grade = grade
+    def initialize(id=nil, name, grade)
+      @id = id
+      @name = name
+      @grade = grade
+    end
+
+    def save
+      sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
   end
 
-  def save
-    sql = <<-SQL 
-      INSERT INTO students (name, grade)
-      VALUES (?, ?)
-    SQL 
+    def self.drop_table
+      sql = "DROP TABLE IF EXISTS students"
+      DB[:conn].execute(sql)
+    end
 
-    DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0] 
-  end
-  end
+    def self.create(name:, album:)
+      student = Student.new(name, album)
+      student.save
+      student
+    end
 
-  def self.create(name:, album:)
-    song = Song.new(name, album)
-    song.save
-    song
-  end 
-   
+    def self.find_by_name(name)
+      sql = "SELECT * FROM students WHERE name = ?"
+      result = DB[:conn].execute(sql, name)[0]
+      Student.new(result[0], result[1], result[2])
+    end
+
+end
